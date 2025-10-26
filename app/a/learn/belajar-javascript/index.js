48a2f4595c3caa1cfbbe7971a534d14edd09cf94
Salman <salmantok.dev@gmail.com>
2025-10-26 19:13:23 +0700

    Update app/a/learn/belajar-javascript/index.js Update app/md/learn/belajar-javascript/index.md Update learn/belajar-javascript/index.html

--- app/a/learn/belajar-javascript/index.js
+++ app/a/learn/belajar-javascript/index.js
@@ -9,3 +9,12 @@ let variableName2;
 variableName2 = 'Salman';
 
 console.log(variableName2);
+
+/**
+ */
+
+let variableName3;
+variableName3 = 'Upin';
+variableName3 = 'Ipin';
+
+console.log(variableName3);
--- app/md/learn/belajar-javascript/index.md
+++ app/md/learn/belajar-javascript/index.md
@@ -10,3 +10,11 @@ variableName2 = 'Salman';
 
 console.log(variableName2);
 ```
+
+```js
+let variableName3;
+variableName3 = 'Upin';
+variableName3 = 'Ipin';
+
+console.log(variableName3);
+```
